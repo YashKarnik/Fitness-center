1f6068662f5ee51abcff152aa7ee447168fff505
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-10-29 19:36:15 +0530

    add:new fields for register and form validation

---
+++ register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Log In</title>
+		<link rel="stylesheet" href="register.css" />
+	</head>
+	<script>
+
+</script>
+	<body>
+	
+		<div class="container">
+			<h1>Register</h1>
+			<form  method="POST" action="success.php">
+				<input
+					type="text"
+					name="username"
+					placeholder="Username"
+				/>
+				<input
+					type="number"
+					name="age"
+					placeholder="Age"
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="Email"
+				/>
+				
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+				/>
+				
+				<button type="submit" name="commit" >Submit</button>
+			</form>
+		</div>
+
+	</body>
+</html>
--- success.php
+++ success.php
@@ -6,19 +6,37 @@
     <title>Document</title>
 </head>
 <style>
-.succces{
-    background_color:green;
-    padding:50px;
-    width:100%;
-    text-align:center;
+.base{
+    font-family:sans-serif;
+width:100%;
+padding:20px;
+text-align:center;
+opacity:0.8;
+border-radius:5px;
 }
+
+.error {
+    background-color:red;
+}
+.success {
+    background-color:green;
+}
+
 </style>
 <body>
     <?php
+    if(empty($_POST['age']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['email'])) {
+        echo "<h1 class='error base'>Error....Empty fields disallowed!!</h1>";
+    }
+    else if(empty(strpos($_POST['password'],'@')))  {
+        echo "<h1 class='error base'>Error....Password must have @!!</h1>";
+    }
+    else{
         $conn = new mysqli('localhost','root','','exercise_center_db') or die('Unable to connect');
-        $query=sprintf("INSERT INTO user VALUES ('%s','%s')",$_POST['login'],$_POST['password']);
+        $query=sprintf("INSERT INTO userdetails (age,email,password,username) VALUES ('%u','%s','%s','%s')",$_POST['age'],$_POST['email'],$_POST['password'],$_POST['username']);
         mysqli_query($conn,$query);
-        echo "<p class='Succes'>Successfully Logged in</p>"
+        echo "<h1 class='success base'>Successfully registered</h1>";
+    }
         
     ?>
 </body>
