b04188616adfb23176713a640cd740e05fd2773d
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-11-27 20:52:45 +0530

    :construction:Depricated module

--- get_usename.php
+++ get_usename.php
@@ -2,6 +2,7 @@
 
 function getUsername(){
     require './config/db_connect.php';
+    $username='';
     if(isset($_COOKIE['email']))
        { $email=$_COOKIE['email'];
     // echo $email;
@@ -9,9 +10,9 @@ function getUsername(){
     $result=mysqli_query($conn,$query);
     // echo print_r($result);
     $posts = mysqli_fetch_all($result,MYSQLI_ASSOC);
-    $username;
     if(count($posts)>0) $username=$posts[0]['username']; 
-    return $username;}
+}
+return $username;
 
 }
 
