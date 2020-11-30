b72c39616be33c0f2cccba05fa87aa44ed96f1ec
Yash Karnik <yashkarnik2000@gmail.com>
2020-11-30 16:17:03 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,14 +1,15 @@
 <?php
 // include(__DIR__.'\..\db_connect_creds.php');
-// $host=$host;
-// $user=$user;
-// $password=$PASSWORD;
-// $database=$DATABASE_NAME;
+$host="localhost";
+$user="root";
+$password="";
+$database="avbc";
 require_once ("vendor/autoload.php");
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'\\..\\');
-$dotenv->load();
-$conn = new mysqli($_ENV['HOST'],$_ENV['USER'],$_ENV['PASSWORD'],$_ENV['DATABASE']);
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'\\..\\');
+//$dotenv->load();
+//$conn = new mysqli($_ENV['HOST'],$_ENV['USER'],$_ENV['PASSWORD'],$_ENV['DATABASE']);
+$conn = new mysqli($host,$user,$password,$database);
 if(mysqli_connect_errno()) {
     echo 'FAIL'.mysqli_connect_errno();
     mysqli_error($conn);
