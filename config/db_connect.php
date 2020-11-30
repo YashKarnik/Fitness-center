ab53001400ef43213edc04ed072263d919c4c165
Yash Karnik <yashkarnik2000@gmail.com>
2020-11-30 16:18:57 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // include(__DIR__.'\..\db_connect_creds.php');
-$host="localhost";
-$user="root";
-$password="";
-$database="avbc";
+$host="mariadb105-cfa1.euw2.cloud.ametnes.com";
+$user="dQpjwsLEmO";
+$password="8btlbGDGu70rbeWjPQUs";
+$database="2428473479";
 require_once ("vendor/autoload.php");
 
 //$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'\\..\\');
